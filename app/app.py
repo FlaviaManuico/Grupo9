83fa2730f84601b73828e7b0a357d91ca85d8f38
Flavia Manuico <flavia.manuico@utec.edu.pe>
2022-05-12 00:01:08 -0500

    #import y #configuration

--- app/app.py
+++ app/app.py
@@ -1,4 +1,12 @@
 import sys
 from datetime import datetime
 from flask import Flask, jsonify, request, url_for, render_template, redirect, flash, session
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Configuration
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/pizza'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'super secret key'
+db = SQLAlchemy(app)
