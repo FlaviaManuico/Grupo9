d1eacb2b976c572cf8595d98301ad00e9e36f06f
MariaSurco <maria.surco@utec.edu.pe>
2022-05-13 21:01:17 -0500

    imagenes y seccion de pedidos

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 #Configuration
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/pizza'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/pizza'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'super secret key'
 db = SQLAlchemy(app)
